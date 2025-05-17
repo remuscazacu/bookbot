1603aa2c34c5227d00cee3690c68709bc0d11df0
remuscazacu <remus.cazacu@bitsoftware.ro>
2025-05-17 17:04:52 +0300

    L3 - Final Submit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_char, get_sorted_list
 
 
@@ -7,10 +8,14 @@ def get_book_text(filepath):
 
 
 def main():
-    filepath = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     print("================ BOOKBOT =====================")
-    print("Analysing book found at books/frankenstein")
+    print(f"Analysing book found at {filepath}")
     print("---------------- Word Count ------------------")
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
